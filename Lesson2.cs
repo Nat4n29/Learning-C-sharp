2507ef61510cc308eb15db1e56a6479b721d50e1
Nat4n29 <natan111n@hotmail.com>
2023-08-14 18:27:00 -0300

    Lesson 2
    gg

---
+++ Lesson2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Lesson2
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, valor;
+
+            double area, preco;
+
+            Console.WriteLine("Insira a largura do terreno:");
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Insira o comprimento do terreno:");
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Insira o valor do metro quadrado:");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = valor * area;
+
+            Console.WriteLine("Area do terreno: " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Preço do terreno: " + preco.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
