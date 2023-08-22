6700e24caacee4b88b914853aa2030e368f97abc
Nat4n29 <natan111n@hotmail.com>
2023-08-21 23:22:04 -0300

    Learning 18
    gg

--- Lessons/NomeIdadeAltura.cs
+++ Lessons/NomeIdadeAltura.cs
@@ -2,7 +2,7 @@
 using System.Globalization;
 using System.Runtime.Serialization;
 
-class Program
+class NomeIdadeALtura
 {
 
     static void Main(string[] args)
@@ -32,7 +32,7 @@ class Program
             altura[i] = double.Parse(input[2], CultureInfo.InvariantCulture);
         }
 
-        for (int i = 0;i < N; i++) //porcentagem.
+        for (int i = 0; i < N; i++) //porcentagem.
         {
 
             if (idade[i] < 16)
@@ -50,7 +50,7 @@ class Program
         porcent = menor * 100.0 / N;
 
         Console.WriteLine("Altura media: " + altMed.ToString("F2", CultureInfo.InvariantCulture));
-        Console.WriteLine("Pessoas com menos de 16 anos: " +  porcent.ToString("F1", CultureInfo.InvariantCulture) + "%");
+        Console.WriteLine("Pessoas com menos de 16 anos: " + porcent.ToString("F1", CultureInfo.InvariantCulture) + "%");
     }
 
 }
