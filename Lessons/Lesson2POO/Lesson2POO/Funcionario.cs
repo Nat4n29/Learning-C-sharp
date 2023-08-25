18ce50645d05e6307eb28cdf25b5d0a04faa0131
Nat4n29 <natan111n@hotmail.com>
2023-08-25 20:33:41 -0300

    Learning 22
    gg

---
+++ Lessons/Lesson2POO/Lesson2POO/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2POO
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem) / 100.00;
+        }
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Lessons/Lesson2POO/Lesson2POO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Lesson2POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+
+            Console.Write("Salario Bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionario: " + f.Nome + ", $ " + f.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture));
+            
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salario: ");
+            double prct = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.AumentarSalario(prct);
+
+            Console.WriteLine("Dados atualizados: " + f.Nome + ", $ " + f.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
