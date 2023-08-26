fe4b6e4ceed15dc5baf48c2d016614453b38a04b
Nat4n29 <natan111n@hotmail.com>
2023-08-25 21:24:12 -0300

    Learning 23
    gg

---
+++ Lessons/Lesson3POO/Lesson3POO/Aluno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3POO
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double A, B, C;
+
+        public double NotaFinal()
+        {
+            return A + B + C;
+        }
+
+        public void Resultado()
+        {
+            double resultado = 60 - NotaFinal();
+            if(NotaFinal() < 60)
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + resultado.ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+            else
+            {
+                Console.WriteLine("APROVADO");
+            }
+        }
+    }
+}
---
+++ Lessons/Lesson3POO/Lesson3POO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Lesson3POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            a.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as tres notas do aluno:");
+            a.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = " + a.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            a.Resultado();
+        }
+    }
+}
