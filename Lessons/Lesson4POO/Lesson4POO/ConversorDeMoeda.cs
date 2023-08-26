92f60747c17eb7a09ed7c683c9abf8f52034d60a
Nat4n29 <natan111n@hotmail.com>
2023-08-26 01:32:38 -0300

    Learning 24
    gg

---
+++ Lessons/Lesson4POO/Lesson4POO/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4POO
+{
+    internal class ConversorDeMoeda
+    {
+        public static double Conversor(double dolar, double quant)
+        {
+            double r = dolar * quant;
+            return r + (r * 6 / 100);
+        }
+    }
+}
---
+++ Lessons/Lesson4POO/Lesson4POO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Lesson4POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolar, quantidade;
+
+            Console.Write("Qual é a cotação do dólar? ");
+            dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais = " + ConversorDeMoeda.Conversor(dolar, quantidade).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
