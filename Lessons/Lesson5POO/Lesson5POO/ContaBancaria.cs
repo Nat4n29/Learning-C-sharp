9ed1cc05e042cfc36372b0e75e11a9e97ba5fee1
Nat4n29 <natan111n@hotmail.com>
2023-09-01 19:16:10 -0300

    Learning 26
    gg

---
+++ Lessons/Lesson5POO/Lesson5POO/ContaBancaria.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5POO
+{
+    internal class ContaBancaria
+    {
+        public int ContaNum { get; private set; }
+        public string Nome { get; private set; }
+        public double Valor { get; private set; }
+        public string Dados { get; private set; }
+
+        
+
+        public ContaBancaria(int conta, string nome, double valor)
+        {
+            ContaNum = conta;
+            Nome = nome;
+            Valor = valor;
+            Dados = "Conta " + ContaNum + ", Titular: " + Nome + ", Saldo: $ " + Valor.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public double DepInic(double valor)
+        {
+            return Valor += valor;
+        }
+
+        public double Deposito(double depoValor)
+        {
+            return Valor += depoValor;
+        }
+
+        public double Saque(double saqValor)
+        {
+            Valor -= saqValor;
+
+            return Valor -= 5.0;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Lessons/Lesson5POO/Lesson5POO/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+
+namespace Lesson5POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depoInic;
+            double depoValor;
+            double saqValor;
+            char havera;
+
+            ContaBancaria cb = new ContaBancaria(0000, "NaN", 0.00);
+
+            Console.Write("Entre o número da conta: ");
+            int contaNum = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Haverá deposito inicial (s/n)? ");
+            havera = char.Parse(Console.ReadLine());
+            cb = new ContaBancaria(contaNum, nome, cb.Valor);
+
+            if (havera == 's')
+            {
+                Console.Write("Entre o valor de deposito inicial: ");
+                depoInic = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                cb.DepInic(depoInic);
+                cb = new ContaBancaria(cb.ContaNum, cb.Nome, cb.Valor);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(cb.Dados);
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para deposito: ");
+            depoValor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cb.Deposito(depoValor);
+            cb = new ContaBancaria(cb.ContaNum, cb.Nome, cb.Valor);
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(cb.Dados);
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para saque: ");
+            saqValor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cb.Saque(saqValor);
+            cb = new ContaBancaria(cb.ContaNum, cb.Nome, cb.Valor);
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(cb.Dados);
+        }
+    }
+}
