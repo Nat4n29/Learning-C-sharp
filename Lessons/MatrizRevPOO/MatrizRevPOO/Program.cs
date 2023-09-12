1c8c035f5bca4088eff302cea93ec0b5fbed3989
Nat4n29 <natan111n@hotmail.com>
2023-09-12 15:01:51 -0300

    Learning 29
    gg

---
+++ Lessons/MatrizRevPOO/MatrizRevPOO/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MatrizRevPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n,n];
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] num = Console.ReadLine().Split(' ');
+                for(int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(num[j]);
+                }
+            }
+            Console.WriteLine("Main diagonal:");
+            for(int i = 0;i < n; i++)
+            {
+                Console.Write(mat[i,i] + " ");
+            }
+            Console.WriteLine();
+
+            int negNum = 0;
+            for(int i = 0;i < n; i++)
+            {
+                for(int j = 0;j < n; j++)
+                {
+                    if (mat[i,j] < 0)
+                    {
+                        negNum++;
+                    }
+                }
+            }
+            Console.WriteLine($"Negative numbers = {negNum}");
+        }
+    }
+}
