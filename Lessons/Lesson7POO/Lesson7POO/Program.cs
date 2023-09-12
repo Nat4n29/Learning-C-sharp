15a403d2d5926987b18a68d757fec995610a466b
Nat4n29 <natan111n@hotmail.com>
2023-09-12 02:00:16 -0300

    Learning 28
    gg

---
+++ Lessons/Lesson7POO/Lesson7POO/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Lesson7POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            int numEmployees;
+
+            Console.Write("How many employees will be registered? ");
+            numEmployees = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= numEmployees; i++)
+            {
+                Console.WriteLine("Employee #" + i +":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+
+                list.Add(new Employee(id, name, salary));
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter the employee id that will have salary increase : ");
+            int idEmployee = int.Parse(Console.ReadLine());
+            Employee emp = list.Find(x => x.Id == idEmployee);
+
+            if( emp != null )
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This Id do not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+
+            foreach(object obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
--- URI-beecrowd/URI-beecrowd/Program.cs
+++ URI-beecrowd/URI-beecrowd/Program.cs
@@ -1,44 +1,10 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 using System.Globalization;
-using System.Runtime.Serialization;
-using System.Security.Cryptography.X509Certificates;
-using System.Xml;
 
 class Program
 {
-
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        int nota;
-
-        nota = int.Parse(Console.ReadLine());
-
-        Console.WriteLine(Nota(nota));
+        double[] vect = new double[100];
     }
-    static char Nota(int nota)
-    {
-        if(nota == 0)
-        {
-            nota = 'E';
-        }
-        else if(nota >= 1 && nota <= 35)
-        {
-            nota = 'D';
-        }
-        else if (nota >= 36 && nota <= 60)
-        {
-            nota = 'C';
-        }
-        else if (nota >= 61 && nota <= 85)
-        {
-            nota = 'B';
-        }
-        else if (nota >= 86 && nota <= 100)
-        {
-            nota = 'A';
-        }
-        return (char)nota;
-    }
-
 }
