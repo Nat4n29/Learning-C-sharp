f6527ad92dbc2c5dade786284a521d2a95b015cf
Nat4n29 <natan111n@hotmail.com>
2023-10-01 23:52:30 -0300

    Learning 37
    gg

---
+++ Lessons/Lesson15POO/Lesson15POO/Entities/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Lesson15POO.Entities.Exceptions;
+
+namespace Lesson15POO.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (Balance == 0 || Balance < amount)
+            {
+                throw new DomainException("Withdraw error: Not enough balance");
+            }
+            if(amount > WithdrawLimit)
+            {
+                throw new DomainException("Withdraw error: The amount exceeds withdraw limit");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
---
+++ Lessons/Lesson15POO/Lesson15POO/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Lesson15POO.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
