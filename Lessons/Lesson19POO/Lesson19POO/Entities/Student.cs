0811219f9a6062460d01b0bda68fd5635883337e
Nat4n29 <natan111n@hotmail.com>
2023-10-24 02:30:38 -0300

    Learning 41
    gg

---
+++ Lessons/Lesson19POO/Lesson19POO/Entities/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson19POO.Entities
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Student))
+            {
+                return false;
+            }
+
+            Student other = obj as Student;
+
+            return Id.Equals(other.Id);
+        }
+    }
+}
---
+++ Lessons/Lesson19POO/Lesson19POO/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Lesson19POO.Entities;
+
+namespace Lesson19POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                HashSet<Student> courseA = new HashSet<Student>();
+                HashSet<Student> courseB = new HashSet<Student>();
+                HashSet<Student> courseC = new HashSet<Student>();
+
+                Console.Write("How many students for course A? ");
+                int num = int.Parse(Console.ReadLine());
+
+                for(int i = 0; i < num; i++)
+                {
+                    int id = int.Parse((Console.ReadLine()));
+                    courseA.Add(new Student() { Id = id});
+                }
+
+                Console.Write("How many students for course B? ");
+                num = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < num; i++)
+                {
+                    int id = int.Parse((Console.ReadLine()));
+                    courseB.Add(new Student() { Id = id });
+                }
+
+                Console.Write("How many students for course C? ");
+                num = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < num; i++)
+                {
+                    int id = int.Parse((Console.ReadLine()));
+                    courseC.Add(new Student() { Id = id });
+                }
+
+                HashSet<Student> totalstudents = new HashSet<Student>();
+                totalstudents.UnionWith(courseA);
+                totalstudents.UnionWith(courseB);
+                totalstudents.UnionWith(courseC);
+
+                Console.WriteLine($"Total students: {totalstudents.Count}");
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
